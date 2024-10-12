814e9c169c0bc889f1cd37680b2d9bb38ad8fb2f
bogdanove <anoth3rx@gmail.com>
2024-10-12 20:46:14 +0300

    ci

--- migration_prod.sh
+++ migration_prod.sh
@@ -3,4 +3,4 @@ source prod.env
 
 MIGRATION_DIR=$(echo "$MIGRATION_DIR" | tr -d '[:space:]')
 
-sleep 2 && goose -dir "${MIGRATION_DIR}" postgres "${MIGRATION_DSN}" up -v
+sleep 2 && goose -dir "${MIGRATION_DIR}" postgres "${MIGRATION_DSN}" up
